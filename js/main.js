ea811260895340f1e928f8d11da7df6795af5390
Jenniferyaya <jenniferfalanga@Jennifers-MacBook-Pro.local>
2016-03-22 13:09:49 -0400

    Css feature added, finished with javascript extras.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 var $input = $('#todo-input');
-var $list = $('.todo-list');
+var $list = $('.list');
 
-$('#todo-list').on('submit', function (e) {
+$('form').on('submit', function (e) {
   var $li = $('<li>');
   e.preventDefault();
   $li.html($input.val());
   $list.append($li);
+});
+
+$('ul').on('click', 'li', function (e) {
+  $(this).toggleClass('cross');
 });
